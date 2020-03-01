90d14b25c35ff56792bcf75440b10f0b471875ad
terzoc <doctort123@gmail.com>
2020-03-01 14:25:38 -0500

    added some stuff lamo

--- src/Dictionay.java
+++ src/Dictionay.java
@@ -1,10 +1,23 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.ScrollPane;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ButtonGroup;
 
 public class Dictionay {
 
-	private JFrame frame;
+	private JFrame frmDictionary;
+	private JTextField txtSearch;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -14,7 +27,7 @@ public class Dictionay {
 			public void run() {
 				try {
 					Dictionay window = new Dictionay();
-					window.frame.setVisible(true);
+					window.frmDictionary.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,9 +46,49 @@ public class Dictionay {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 553, 328);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDictionary = new JFrame();
+		frmDictionary.setResizable(false);
+		frmDictionary.setTitle("Dictionary");
+		frmDictionary.setBounds(100, 100, 800, 600);
+		frmDictionary.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDictionary.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(10, 11, 89, 23);
+		frmDictionary.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Remove");
+		btnNewButton_1.setBounds(108, 11, 89, 23);
+		frmDictionary.getContentPane().add(btnNewButton_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(490, 332, -57, -98);
+		frmDictionary.getContentPane().add(scrollPane);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane_1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane_1.setBounds(22, 110, 160, 322);
+		frmDictionary.getContentPane().add(scrollPane_1);
+		
+		txtSearch = new JTextField();
+		txtSearch.setToolTipText("");
+		txtSearch.setBounds(10, 51, 179, 20);
+		frmDictionary.getContentPane().add(txtSearch);
+		txtSearch.setColumns(10);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Asc");
+		buttonGroup.add(rdbtnNewRadioButton);
+		rdbtnNewRadioButton.setBounds(22, 78, 59, 23);
+		frmDictionary.getContentPane().add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Desc");
+		buttonGroup.add(rdbtnNewRadioButton_1);
+		rdbtnNewRadioButton_1.setBounds(96, 78, 59, 23);
+		frmDictionary.getContentPane().add(rdbtnNewRadioButton_1);
 	}
-
 }
