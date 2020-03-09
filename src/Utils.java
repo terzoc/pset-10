3499004750fa93c10aa9a0a9121a41b5017a56a3
terzoc <doctort123@gmail.com>
2020-03-08 20:12:04 -0400

    fixed white space

--- src/Utils.java
+++ src/Utils.java
@@ -1,7 +1,7 @@
 import javax.swing.DefaultListModel;
 
 public class Utils {
-//		Returns the classpath for json file
+//    Returns the classpath for json file
     public static String getClasspathDir() {
         String classpath = System.getProperty("java.class.path", ".");
         boolean windows = false;
@@ -27,34 +27,34 @@ public class Utils {
             }
             return classpathDirectory;
         }
-    }    
+    }
 
 //    Sorts DLM in ascending order
     public static DefaultListModel<String> sortWordsAsc(DefaultListModel<String> listOfWords) {
-    	String temp;
-    	int n = listOfWords.getSize();
-    	for (int i = 0; i < n; i++) {
-    		for (int j = i + 1; j < n; j++) {
+      String temp;
+      int n = listOfWords.getSize();
+      for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
                if ((listOfWords.get(i).compareTo(listOfWords.get(j)) > 0)) {
-            	   temp = listOfWords.get(i);
+                 temp = listOfWords.get(i);
                    listOfWords.set(i, listOfWords.get(j));
                    listOfWords.set(j, temp);
                }
            }
-    	}
-    	return listOfWords;
+      }
+      return listOfWords;
     }
 
-//		Reverse the order of a DLM
+//    Reverse the order of a DLM
     public static DefaultListModel<String> reverseOrder(DefaultListModel<String> words) {
-    	DefaultListModel<String> b = new DefaultListModel<String>(); 
-    	int n = words.getSize();
-    	int j = n; 
-    	for (int i = 0; i < n; i++) { 
-    		b.addElement(words.get(j-1)); 
-    		j = j - 1; 
-    	} 
-    	return b;
-    } 
+      DefaultListModel<String> b = new DefaultListModel<String>();
+      int n = words.getSize();
+      int j = n;
+      for (int i = 0; i < n; i++) {
+        b.addElement(words.get(j-1));
+        j = j - 1;
+      }
+      return b;
+    }
 
 }
